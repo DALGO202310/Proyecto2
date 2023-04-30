b39c55e67b23b788283cc416186508a4ae84c90a
dcamachom <D.CAMACHOM@UNIANDES.EDU.CO>
2023-04-29 21:44:16 -0500

    comentariosEnCodigo

--- src/ProblemaP2.java
+++ src/ProblemaP2.java
@@ -36,12 +36,16 @@ public class ProblemaP2 {
                     adyacencias= instancia.agregarConexion(adyacencias, v1, v2, k);
                     rta.add(instancia.esRedundante(adyacencias, m));
                 }
+                instancia.printList(rta);
             }
         }
     }
     
     /**
-     * funcion para inicializar la lista de adyacencias
+     * funcion para inicializar la lista de adyacencias.Es una lista de listas.
+     * Cada posición representa un computador y dentro de cada posición tendrá tuplas represetadas como 
+     * (vertice con que tiene conexión, tipo de conexión (k))
+     * cada posic
      * @param vertices numero de computadores 
      * @return lista de adyacencias vacia
      */
@@ -56,7 +60,8 @@ public class ProblemaP2 {
     }
 
     /**
-     * funcion para inicializar la matriz donde se especifica el tipo de conexion
+     * funcion para inicializar la matriz donde se especifica el estado de redundancia entre todos 
+     * los pares de vertices (computadores) según los tipos de conexiones que hayan entre ellos
      * 0 -> hay conexión con un solo cable (1 o 2)
      * 1 -> hay conexión con los dos cables (1 y 2)
      * 2 -> no hay ninguna conexión 
@@ -109,7 +114,6 @@ public class ProblemaP2 {
     public boolean hayCamino (ArrayList<ArrayList<int[]>> adyacencias, int a, int b,int k){
         Queue<Integer> cola= new LinkedList<>();
         ArrayList<Integer> result= new ArrayList<>();
-        //se debe arreglar la resta del 1 en la funcion principal
         cola.add(a);
         int n=a;
         ArrayList <int []> list;
@@ -135,7 +139,10 @@ public class ProblemaP2 {
      * @param adyacencias lista de adyacencias
      * @param a vertice a
      * @param b vertice b
-     * @return boolean que indica si es redundante 
+     * @return int que indica si es redundante
+     * 0 -> hay conexión con un solo cable (1 o 2) - no redundante
+     * 1 -> hay conexión con los dos cables (1 y 2) - redundante
+     * 2 -> no hay ninguna conexión - redundante
      * p1= ¿Hay camino de a-b con 1?
      * p2= ¿Hay camino de a-b con 2?
      */
@@ -151,6 +158,12 @@ public class ProblemaP2 {
         }
     }
 
+    /**
+     * funcion para determinar si la red es redundante
+     * @param adyacencias lista de adyacencias
+     * @param m matriz que indica el tipo de redundancia
+     * @return boolean 
+     */
     public boolean esRedundante (ArrayList<ArrayList<int[]>> adyacencias, int [][] m){
         for (int i=0; i<adyacencias.size();i++){
             for (int j=0; j<adyacencias.size();j++){
@@ -167,7 +180,7 @@ public class ProblemaP2 {
     }
 
     public void printList (ArrayList<Integer> list){
-        
+
     }
 
 }
